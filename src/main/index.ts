2dec06562aa65a3e58414cb85e3a1ddd3c61064f
goldenaman-dev <amannkr101@gmail.com>
2026-06-18 15:41:00 +0530

    release version 1.0

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,25 +1,19 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { app, shell, BrowserWindow, ipcMain, dialog } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+
 import { initializeDatabase } from './server/database'
 import { registerAuthHandlers } from './server/apis/auth'
 import { registerMenuHandlers } from './server/apis/menu'
 
-app.whenReady().then(() => {
-  initializeDatabase()
+import { autoUpdater } from "electron-updater"
 
-  console.log("database initilaised")
+let mainWindow: BrowserWindow | null = null
 
-  registerAuthHandlers()
-  registerMenuHandlers()
-
-  createWindow()
-})
-
+// -------------------- CREATE WINDOW --------------------
 function createWindow(): void {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
     show: false,
@@ -32,7 +26,7 @@ function createWindow(): void {
   })
 
   mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
+    mainWindow?.show()
   })
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
@@ -40,8 +34,6 @@ function createWindow(): void {
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
@@ -49,40 +41,58 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// -------------------- AUTO UPDATER --------------------
+function setupAutoUpdater() {
+  autoUpdater.checkForUpdatesAndNotify()
+
+  autoUpdater.on("update-available", () => {
+    console.log("Update available")
+  })
+
+  autoUpdater.on("update-downloaded", () => {
+    dialog.showMessageBox({
+      type: "info",
+      title: "Update Ready",
+      message: "New version downloaded. Restart to apply update?",
+      buttons: ["Restart", "Later"]
+    }).then(result => {
+      if (result.response === 0) {
+        autoUpdater.quitAndInstall()
+      }
+    })
+  })
+}
+
+// -------------------- APP READY --------------------
 app.whenReady().then(() => {
-  // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
 
-  // IPC test
+  // IPC
   ipcMain.on('ping', () => console.log('pong'))
 
+  // INIT APP LOGIC
+  initializeDatabase()
+  registerAuthHandlers()
+  registerMenuHandlers()
+
+  // CREATE WINDOW (ONLY ONCE)
   createWindow()
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+  // AUTO UPDATER
+  setupAutoUpdater()
+
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// -------------------- CLOSE APP --------------------
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
